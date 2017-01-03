f26efac21968b0a906cf649aebe8a6f3689b064c
Helio Frota <00hf11@gmail.com>
2017-01-02 22:25:43 -0300

    comments

--- hw.asm
+++ hw.asm
@@ -1,9 +1,15 @@
+; the variable area
+; for declarations and initializations.
 section .data
 msg db 'hello world', 0AH
 len equ $-msg
-
+; the instruction/statements area.
 section .text
+; the start label where the program will start from.
+; using the global directive, means this can be used
+; externally.
 global _start
+; the program starts here.
 _start: mov edx, len
         mov ecx, msg
         mov ebx, 1
