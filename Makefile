0f40341e8ff2f291ddda3b142c19048a9e79300c
Helio Frota <00hf11@gmail.com>
2017-01-13 02:30:33 -0300

    foo c funtion

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 build: 
 	as -o hw.o hw.s
 	ld -o hw hw.o
+	gcc main.c hwc.c -o main
 
 .PHONY: build
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "hwc.h"
+#include <stdio.h>
+
+int main() {
+  int f = foo(1);
+  printf("%d", f);
+  return f;
+}
