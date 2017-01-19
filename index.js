233285959a1ff056ec494489ca441d3f879026a7
Helio Frota <00hf11@gmail.com>
2017-01-19 15:26:23 -0300

    removing undefined from output

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const addon = require('./build/Release/jsfoo.node');
 
 function jsFoo () {
   console.log('This is a Javascript function.');
-  console.log(addon.jsfoo());
+  addon.jsfoo();
 }
 
 jsFoo();
