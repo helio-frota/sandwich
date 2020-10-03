9dc2f2e35f1fbf1ff21523520f3ecc732f50cc6a
Helio Frota <00hf11@gmail.com>
2020-10-03 07:05:45 -0300

    adding ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {javascriptPart} from './jsPart';
+
+class TypeScriptPart {
+  constructor() {
+    console.log('This is a TypeScript class constructor.');
+    javascriptPart();
+  }
+}
+
+new TypeScriptPart();
--- jsPart.js
+++ jsPart.js
@@ -5,4 +5,6 @@ function javascriptPart () {
   addon.cppPartExportedByNapi();
 }
 
-javascriptPart();
+module.exports = {
+  javascriptPart
+}
